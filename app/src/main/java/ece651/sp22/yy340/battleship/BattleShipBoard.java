ece365a4a1512d40e856c8041c253fa59d6b120e
thirteendian <yyx980325@hotmail.com>
2022-01-15 21:36:21 -0500

    task 0 task 1

---
+++ app/src/main/java/ece651/sp22/yy340/battleship/BattleShipBoard.java
@@ -0,0 +1,35 @@
+package ece651.sp22.yy340.battleship;
+
+  /**
+   * Constructs a BattleShipBoard with the specified width
+   * and height
+   * @param w is the width of the newly constructed board.
+   * @param h is the height of the newly constructed board.
+   * @throws IllegalArgumentException if the width or height are less than or equal to zero.
+   */
+
+public class BattleShipBoard implements Board{
+  private final int width;
+  private final int height;
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public BattleShipBoard  (int w, int h){
+    if (w <= 0) {
+      throw new IllegalArgumentException("BattleShipBoard's width must be positive but is " + w);
+    }
+    if (h <= 0) {
+      throw new IllegalArgumentException("BattleShipBoard's height must be positive but is " + h);
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+
+}
---
+++ app/src/main/java/ece651/sp22/yy340/battleship/Board.java
@@ -0,0 +1,8 @@
+package ece651.sp22.yy340.battleship;
+
+public interface Board {
+
+  public int getWidth();
+  public int getHeight();
+  
+}
