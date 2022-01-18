8b73090ad0ed1092fa592ff71b1d1e0190b9c7f6
thirteendian <yyx980325@hotmail.com>
2022-01-17 23:50:13 -0500

    task 8

--- app/src/main/java/ece651/sp22/yy340/battleship/BoardTextView.java
+++ app/src/main/java/ece651/sp22/yy340/battleship/BoardTextView.java
@@ -1,47 +1,47 @@
 package ece651.sp22.yy340.battleship;
+
 /**
- * This class handles textual display of
- * a Board (i.e., converting it to a string to show
- * to the user).
- * It supports two ways to display the Board:
- * one for the player's own board, and one for the 
- * enemy's board.
+ * This class handles textual display of a Board (i.e., converting it to a
+ * string to show to the user). It supports two ways to display the Board: one
+ * for the player's own board, and one for the enemy's board.
  */
 
 public class BoardTextView {
   /**
    * The Board to display
    */
-  private final Board <Character> toDisplay;
+  private final Board<Character> toDisplay;
+
   /**
    * Constructs a BoardView, given the board it will display.
+   * 
    * @param toDisplay is the Board to display
-   * @throws IllegalArgumentException if the board is larger than 10x26.  
+   * @throws IllegalArgumentException if the board is larger than 10x26.
    */
-  public BoardTextView(Board <Character> toDisplay) {
+  public BoardTextView(Board<Character> toDisplay) {
     this.toDisplay = toDisplay;
     if (toDisplay.getWidth() > 10 || toDisplay.getHeight() > 26) {
       throw new IllegalArgumentException(
           "Board must be no larger than 10x26, but is " + toDisplay.getWidth() + "x" + toDisplay.getHeight());
     }
   }
-  
+
   public String displayMyOwnBoard() {
     StringBuilder ans = new StringBuilder();
     ans.append(makeHeader());
     ans.append(makeBoard());
     ans.append(makeHeader());
-    return ans.toString(); //this is a placeholder for the moment
- }
+    return ans.toString(); // this is a placeholder for the moment
+  }
 
-   /**
+  /**
    * This makes the header line, e.g. 0|1|2|3|4\n
    * 
    * @return the String that is the header line for the given board
    */
   String makeHeader() {
     StringBuilder ans = new StringBuilder("  "); // README shows two spaces at
-    String sep=""; //start with nothing to separate, then switch to | to separate
+    String sep = ""; // start with nothing to separate, then switch to | to separate
     for (int i = 0; i < toDisplay.getWidth(); i++) {
       ans.append(sep);
       ans.append(i);
@@ -51,24 +51,35 @@ public class BoardTextView {
     return ans.toString();
   }
 
-   /**
+  /**
    * This makes the Board context, e.g., A | A\n
    *
    * @return the String that is the Test board for the given board
    */
 
-                                                 
-  String makeBoard(){
+  String makeBoard() {
     StringBuilder ans = new StringBuilder();
     char alphabetic = 'A';
-    for(int i =0; i < toDisplay.getHeight(); i++){
-      ans.append((char) (alphabetic + i));
-      ans.append(" ");
-      for(int j =0; j< toDisplay.getWidth()-1; j++){
-        ans.append(" |");
-      }
+    for (int row = 0; row < toDisplay.getHeight(); row++) {
+      ans.append((char) (alphabetic + row));
       ans.append("  ");
-      ans.append((char)(alphabetic + i));
+      /*if (toDisplay.whatIsAt(new Coordinate(row, 0)) == null) {
+        ans.append(' ');
+      } else {
+        ans.append(toDisplay.whatIsAt(new Coordinate(row, 0)));
+      }*/
+
+      for (int column = 1; column < toDisplay.getWidth(); column++) {
+        //Note that Column start from 1
+        ans.append("|"); // Two space -> one space
+        if (toDisplay.whatIsAt(new Coordinate(row, column)) == null) {
+          ans.append(' ');
+        } else {
+          ans.append(toDisplay.whatIsAt(new Coordinate(row, column)));
+        }
+      }
+      ans.append(" ");// two space -> one space
+      ans.append((char) (alphabetic + row));
       ans.append('\n');
     }
     return ans.toString();
--- app/src/test/java/ece651/sp22/yy340/battleship/BoardTextViewTest.java
+++ app/src/test/java/ece651/sp22/yy340/battleship/BoardTextViewTest.java
@@ -2,6 +2,8 @@ package ece651.sp22.yy340.battleship;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 
 public class BoardTextViewTest {
@@ -12,6 +14,28 @@ public class BoardTextViewTest {
     String expectedHeader = "  0|1\n";
     assertEquals(expectedHeader, view.makeHeader());
     String expected = expectedHeader + "A  |  A\n" + "B  |  B\n" + expectedHeader;
+    assertEquals(expected, view.displayMyOwnBoard());
+  }
+
+  @Test
+  public void test_display_non_empty_4by3() {
+    LinkedList<Ship<Character>> shipList = new LinkedList<>();
+    BasicShip s1 = new BasicShip(new Coordinate(0, 3));
+    BasicShip s2 = new BasicShip(new Coordinate(2, 1));
+    shipList.add(s1);
+    shipList.add(s2);
+    Board<Character> b2 = new BattleShipBoard<Character>(4, 3);    
+    for (Ship<Character> ship : shipList){
+      b2.tryAddShip(ship);
+    }
+    String expectedHeader = "  0|1|2|3\n";    
+    BoardTextView view = new BoardTextView(b2);
+    assertEquals(expectedHeader, view.makeHeader());
+    String expected = expectedHeader +
+      "A  | | |s A\n" +
+      "B  | | |  B\n" +
+      "C  |s| |  C\n" +
+      expectedHeader;
     assertEquals(expected, view.displayMyOwnBoard());
   }
 
